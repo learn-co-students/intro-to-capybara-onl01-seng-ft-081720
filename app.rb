5e2f96deebfbbeb68f73674379e148ceb5081c77
Newland-A <ajn252@gmail.com>
2020-09-25 15:03:14 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 class Application < Sinatra::Base
   # Write your code here!
+  get '/' do
+    erb :index
+  end
+
+  get '/index' do
+    erb :index
+  end
+# uses post to actually greet the user
+  post '/greet' do
+    erb :greet
+  end
 
 end
