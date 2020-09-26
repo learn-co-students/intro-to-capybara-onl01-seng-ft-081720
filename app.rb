43462d8f0e381f9d50a0601f9135864db2279a30
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-26 05:42:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 class Application < Sinatra::Base
-  # Write your code here!
 
+# tells the application to render, or deliver to the user's browser  
+  get '/' do 
+    erb :index 
+  end 
+  
+    # New route to respond to the form submission
+  post '/greet' do
+    erb :greet
+  end
+end
 end
